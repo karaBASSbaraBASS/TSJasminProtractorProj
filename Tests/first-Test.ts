3a9dbe6d5fc2365a6c1ec20a871ac965246a97a6
karaBASSbaraBASS <evgen.mikhalov@gmail.com>
2021-03-15 03:34:20 +0200

    created test for angular project

--- Tests/first-Test.ts
+++ Tests/first-Test.ts
@@ -1,37 +1,54 @@
 import { browser, by, element, protractor } from "protractor";
 
-describe("Search", () => {
-  it("Open google and find a text", async () => {
-    // Создаем объект для работы с ожиданиями
-    let EC = protractor.ExpectedConditions;
+//Можно загрузить страничку
+describe("Regression", () => {
+  // Создаем объект для работы с ожиданиями
+  let EC = protractor.ExpectedConditions;
+  let input_first = element(by.css("input[ng-model='first']"));
+  let input_second = element(by.css("input[ng-model='second']"));
+  let gobutton = element(by.id("gobutton"));
+  let result = element(by.className("ng-binding"));
 
-    // выключаем проверку на AngularJS
-    await browser.waitForAngularEnabled(false);
+  // beforeEach(function () {
+  //   browser.get("http://juliemr.github.io/protractor-demo/");
+  // });
 
-    // открываем страницу Google
-    await browser.get("https://www.google.com/");
+  it("should be available", async () => {
+    // включаем проверку на AngularJS
+    await browser.waitForAngularEnabled(true);
 
-    // создаем элемент по css = input[role='combobox']
-    let input_button = element(by.css("input[role='combobox']"));
+    // Перейти на страничку с тестовым приложением
+    await browser.get("http://juliemr.github.io/protractor-demo/");
 
-    // ждем появление этого элемента (события presenceOf)
-    await browser.wait(EC.presenceOf(input_button), 5000);
-
-    // пишем в элемент текст “protractor”
-    await input_button.sendKeys("protractor");
-
-    // создаем элемент кнопки сабмита по css
-    let submit_button = element(
-      by.css(".FPdoLc input[type='submit'][name='btnK']")
-    );
-
-    // дожидаемся его появления на странице (не обязательно, ведь мы уже дождались input-элемента, значит страница загрузилась)
-    await browser.wait(EC.presenceOf(submit_button), 5000);
-
-    // кликаем по кнопке сабмита
-    await submit_button.click();
-
-    // ждем, когда URL будет содержать текст 'protractor'
-    await browser.wait(EC.urlContains("protractor"), 5000);
+    // Проверяем запущено ли приложение, путем сравнения тайтла
+    await browser.wait(EC.titleContains("Super Calculator"), 5000);
   });
+
+  //В поля можна ввести значения
+  //Нажатие на кнопку ГО вызывает появление результата
+
+  it("should add one and three", async () => {
+    // Перейти на страничку с тестовым приложением
+    await browser.get("http://juliemr.github.io/protractor-demo/");
+
+    // Вводим тестовые числа
+    await input_first.sendKeys("1");
+    await input_second.sendKeys("3");
+
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "4"), 5000);
+  });
+  it("it should divide 9 on 3", async () => {});
+  it("it should substract 3 from 6", async () => {});
+  it("it should multiply 5 on 5", async () => {});
+  it("it should take a percentage", async () => {});
+
+  it("it should work with fractions", async () => {});
+
+  it("should work with minus numbers", async () => {});
 });
+
+//операция с двойным минусом
+//16 символов поле запятой не больше
