543dc1208ff6dcf15cf4bc63f274945bb76b4b5f
karaBASSbaraBASS <evgen.mikhalov@gmail.com>
2021-04-03 20:51:31 +0300

    math function added, % didnt work known issue

--- Tests/first-Test.ts
+++ Tests/first-Test.ts
@@ -1,36 +1,36 @@
 import { browser, by, element, protractor } from "protractor";
 
 //Можно загрузить страничку
-describe("Regression", () => {
+describe("calculator on main page", () => {
   // Создаем объект для работы с ожиданиями
   let EC = protractor.ExpectedConditions;
   let input_first = element(by.css("input[ng-model='first']"));
   let input_second = element(by.css("input[ng-model='second']"));
   let gobutton = element(by.id("gobutton"));
-  let result = element(by.className("ng-binding"));
+  let result = element(by.css("h2[class=ng-binding]"));
+  let AllOption = element(
+    by.css("select[ng-options='value for (key, value) in operators']")
+  );
+  let ADDITION = element(by.css("option[value='ADDITION']"));
+  let DIVISION = element(by.css("option[value='DIVISION']"));
+  let MODULO = element(by.css(".option[value='MODULO']"));
+  let MULTIPLICATION = element(by.css("option[value='MULTIPLICATION']"));
+  let SUBTRACTION = element(by.css("option[value='SUBTRACTION']"));
 
-  // beforeEach(function () {
-  //   browser.get("http://juliemr.github.io/protractor-demo/");
-  // });
+  beforeEach(async function () {
+    // Перейти на страничку с тестовым приложением перед каждым тестом
+    await browser.get("http://juliemr.github.io/protractor-demo/");
+  });
 
   it("should be available", async () => {
     // включаем проверку на AngularJS
     await browser.waitForAngularEnabled(true);
 
-    // Перейти на страничку с тестовым приложением
-    await browser.get("http://juliemr.github.io/protractor-demo/");
-
     // Проверяем запущено ли приложение, путем сравнения тайтла
     await browser.wait(EC.titleContains("Super Calculator"), 5000);
   });
 
-  //В поля можна ввести значения
-  //Нажатие на кнопку ГО вызывает появление результата
-
-  it("should add one and three", async () => {
-    // Перейти на страничку с тестовым приложением
-    await browser.get("http://juliemr.github.io/protractor-demo/");
-
+  it("should be able to add 1 and 3", async () => {
     // Вводим тестовые числа
     await input_first.sendKeys("1");
     await input_second.sendKeys("3");
@@ -40,14 +40,83 @@ describe("Regression", () => {
     // Проверяем результат сумирования
     await browser.wait(EC.textToBePresentInElement(result, "4"), 5000);
   });
-  it("it should divide 9 on 3", async () => {});
-  it("it should substract 3 from 6", async () => {});
-  it("it should multiply 5 on 5", async () => {});
-  it("it should take a percentage", async () => {});
+  it("should be able to divide 9 on 3", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("9");
+    await input_second.sendKeys("3");
+    // выбрать деление
+    await AllOption.click();
+    await DIVISION.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "3"), 5000);
+  });
+  it("should be able to substract 3 from 6", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("6");
+    await input_second.sendKeys("3");
+    // выбрать деление
+    await AllOption.click();
+    await SUBTRACTION.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "3"), 5000);
+  });
+  it("should be able to multiply 5 on 5", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("5");
+    await input_second.sendKeys("5");
+    // выбрать деление
+    await AllOption.click();
+    await MULTIPLICATION.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "25"), 5000);
+  });
+  it("should be able to take a percentage", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("25");
+    await input_second.sendKeys("100");
+    // выбрать деление
+    await AllOption.click();
+    await MODULO.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "4"), 5000);
+  });
 
-  it("it should work with fractions", async () => {});
+  it("should work with fractions", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("1");
+    await input_second.sendKeys("3");
+    // выбрать деление
+    await AllOption.click();
+    await DIVISION.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(
+      EC.textToBePresentInElement(result, "0.3333333333"),
+      5000
+    );
+  });
 
-  it("should work with minus numbers", async () => {});
+  it("should work with minus numbers", async () => {
+    // Вводим тестовые числа
+    await input_first.sendKeys("-1");
+    await input_second.sendKeys("3");
+    // выбрать деление
+    await AllOption.click();
+    await ADDITION.click();
+    // Проверяем работает ли кнопка go
+    await gobutton.click();
+    // Проверяем результат сумирования
+    await browser.wait(EC.textToBePresentInElement(result, "2"), 5000);
+  });
 });
 
 //операция с двойным минусом
